0df2758c686506a250ba0425c506f6bac041d5d3
Khay150 <fvanotti15@gmail.com>
2024-10-31 10:35:42 -0300

    ,

--- ejercicioA.cpp
+++ ejercicioA.cpp
@@ -76,6 +76,7 @@ vector<int> memo;
 void dfs(int v, int p = -1) {
     estado[v] = EMPECE_A_VER;
     for (int u : ady[v]) {
+        if (u == p) continue; 
         if (estado[u] == NO_LO_VI) {
             tree_edges[v].push_back(u);
             padre[u] = v;
@@ -88,19 +89,155 @@ void dfs(int v, int p = -1) {
             }
         }
     }
+
     estado[v] = TERMINE_DE_VER;
 }
 
 int cubren(int v, int p) {
     if (memo[v] != -1) return memo[v];
     int res = 0;
+    // Contar la cantidad de hijos de v
+    int hijos_count = 0;
     for (int hijo : tree_edges[v]) {
         if (hijo != p) {
             res += cubren(hijo, v);
+            hijos_count++; // Contar los hijos
         }
     }
     res += back_edges_con_extremo_superior_en[v];
-    res -= back_edges_con_extremo_inferior_en[v];
+    res -= (back_edges_con_extremo_inferior_en[v] - hijos_count );
     memo[v] = res;
     return res;
+}
+
+
+void Kruskal_Mod( int n, int m, vector<tuple<int, int, int>>& aristas, vector<int>& indices, vector<string>& res, vector<vector<int>>& adj)  {
+
+    DisjointSet dsu(n);
+
+    //ordenar las aristas por peso
+    sort(indices.begin(), indices.end(), [&](int index1, int index2) {
+
+        return get<0>(aristas[index1]) < get<0>(aristas[index2]);
+
+    });
+     // Procesamos las aristas por grupos de peso
+    for (int i = 0; i < m;) {
+
+        int peso_actual = get<0>(aristas[indices[i]]);
+
+        vector<int> grupo_actual;
+
+        while (i < m && (get<0>(aristas[indices[i]]) == peso_actual))
+        {
+            grupo_actual.push_back(indices[i]);
+            i++;
+        }
+
+
+        // Crear una copia del DSU
+        DisjointSet dsu_temporal = dsu;
+
+        if (grupo_actual.size() == 1) {
+
+            int index = grupo_actual[0];
+
+            int u = get<1>(aristas[index]);
+            int v = get<2>(aristas[index]);
+
+            if (dsu_temporal.findSet(u) != dsu_temporal.findSet(v)) {
+
+                res[index] = "any";
+
+                dsu.unionByRank(u, v);
+
+            } else {
+
+                res[index] = "none";
+
+            }
+        }
+        else { //Tengo problemas de memoria en esta parte de Kruskal
+
+            for (int index : grupo_actual) {
+
+                int u = get<1>(aristas[index]);
+                int v = get<2>(aristas[index]);
+
+                if (dsu_temporal.findSet(u) != dsu_temporal.findSet(v)) {
+
+                    dsu_temporal.unionByRank(u, v);
+
+                    estado.assign(n + 1, NO_LO_VI); 
+                    back_edges_con_extremo_superior_en.assign(n + 1, 0); 
+                    back_edges_con_extremo_inferior_en.assign(n + 1, 0); 
+                    memo.assign(n + 1, -1); 
+                    tree_edges.assign(n + 1, vector<int>()); 
+                    padre.assign(n + 1, -1);
+
+                    dfs(u); // Hacemos DFS en el grafo que se crea con el DSU temporal
+
+                    if (cubren(u, -1) == 0) {
+                        res[index] = "any";
+                    }
+
+                    else {
+                        res[index] = "at least one";
+                    }
+
+                }
+
+                else {
+                    res[index] = "none";
+                } 
+            }
+            // Actualizamos el DSU principal con las aristas que no generaron ciclos
+            for (int index : grupo_actual) {
+
+                int u = get<1>(aristas[index]);
+                int v = get<2>(aristas[index]);
+
+                if (dsu_temporal.findSet(u) != dsu_temporal.findSet(v)) {
+                    dsu.unionByRank(u, v);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+
+    int n, m;
+    cout << ":";
+    cin >> n >> m;
+
+    vector<tuple<int, int, int>> aristas(m);
+    vector<int> indices(m);
+    vector<string> res(m);
+    vector<vector<int>> ady(n+1);
+
+    estado.assign(n + 1, NO_LO_VI); 
+    back_edges_con_extremo_superior_en.assign(n + 1, 0); 
+    back_edges_con_extremo_inferior_en.assign(n + 1, 0); 
+    memo.assign(n + 1, -1); 
+    tree_edges.assign(n + 1, vector<int>()); 
+    padre.assign(n + 1, -1);
+
+    for (int i = 0; i < m; i++) {
+        int a, b, w;
+        cout << ":";
+        cin >> a >> b >> w;
+        aristas[i] = make_tuple(w, a, b);
+        indices[i] = i;
+        ady[a].push_back(b);
+        ady[b].push_back(a);
+    }
+
+    Kruskal_Mod(n, m, aristas, indices, res, ady);
+
+    for(int i = 0; i < m; i++) {
+         cout << res[i] << endl;
+    }
+    
+    return 0;
 }
